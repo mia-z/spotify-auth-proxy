4267eda56d0ece3e67d0b3caf6955192315af94b
miaz <iperish.x@gmail.com>
2022-02-20 19:31:58 +0100

    added a tauri redirect

--- pages/api/auth/callback.ts
+++ pages/api/auth/callback.ts
@@ -35,7 +35,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     });
 
     if (authRes.status === 200) {
-        res.redirect("/");
+        res.redirect("tauri://");
     } else {
         res.send("fail");
         res.status(500);
