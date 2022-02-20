0e434a8e42709306cebd68fefa602f16e73a2bcc
miaz <iperish.x@gmail.com>
2022-02-20 19:23:18 +0100

    added token parsing endpoint; added proper redirect to callback

--- pages/api/auth/callback.ts
+++ pages/api/auth/callback.ts
@@ -35,8 +35,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     });
 
     if (authRes.status === 200) {
-        res.json(authRes.data);
-        res.end();
+        res.redirect("/");
     } else {
         res.send("fail");
         res.status(500);
