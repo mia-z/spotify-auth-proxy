66a71a3e9fcaf53ab6a77c3b56aeb454407c29cd
miaz <iperish.x@gmail.com>
2022-02-20 22:25:42 +0100

    redirect testing

--- pages/api/auth/callback.ts
+++ pages/api/auth/callback.ts
@@ -35,7 +35,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     });
 
     if (authRes.status === 200) {
-        res.redirect("http://localhost:3000");
+        res.redirect("http://localhost:3000/no-log?token=" + authRes.data.access_token);
     } else {
         res.send("fail");
         res.status(500);
