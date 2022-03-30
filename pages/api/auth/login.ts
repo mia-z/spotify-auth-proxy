73d09458b954d5401db77602a1f5963e08b6944c
mia-z <iperish.x@gmail.com>
2022-03-30 11:50:35 +0200

    Update login.ts

--- pages/api/auth/login.ts
+++ pages/api/auth/login.ts
@@ -22,6 +22,6 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 		state: state
 	});
 
-	res.redirect("https://accounts.spotify.com/authorize/?" + qParams.toString());
-	//res.send("https://accounts.spotify.com/authorize/?" + qParams.toString());
+	//res.redirect("https://accounts.spotify.com/authorize/?" + qParams.toString());
+	res.send("https://accounts.spotify.com/authorize/?" + qParams.toString());
 }
