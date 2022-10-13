af14c70610e9a26bf0bc625441f6c9f566f5a526
Ryan <iperish.x@gmail.com>
2022-10-13 20:26:38 +0200

    added a force dialog for spotify login endpoint'

---
+++ pages/api/auth/login/force.ts
@@ -0,0 +1,27 @@
+import { NextApiRequest, NextApiResponse } from "next"
+import { randomUUID } from "crypto";
+import NextCors from "nextjs-cors";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+
+	await NextCors(req, res, {
+        methods: ["GET", "POST"],
+        // origin: "http://localhost:3000",
+		// origins: ["http://localhost:3000", "https://miaz.xyz", "https://www.miaz.xyz"],
+        origin: ["http://localhost:3000", "http://localhost:8080", "https://miaz.xyz", "https://www.miaz.xyz"],
+        optionsSuccessStatus: 200
+    });
+
+	const scope = "streaming \ user-read-email \ user-read-private";
+	const state = randomUUID();
+	const qParams = new URLSearchParams({
+		response_type: "code",
+		client_id: process.env.SPOTIFY_CLIENT_ID,
+		scope: scope,
+		redirect_uri: "https://spotify-auth-proxy.vercel.app/api/auth/callback",
+		state: state,
+		show_dialog: "true"
+	});
+
+	res.redirect("https://accounts.spotify.com/authorize/?" + qParams.toString());
+}
